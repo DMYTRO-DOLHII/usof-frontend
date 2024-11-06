433f750baceb0d40e2054c72f4a605514121f04b
Dmytro <dolgiy.dmitriyp@gmail.com>
2024-11-06 11:46:40 +0100

    Minor changes

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import Header from '../../components/Header';
-import Footer from '../../components/Footer';
+import Footer from '../../components/Footer'; 
 import './Home.css';
 import '../../App.css';
 import { decodeTokenLogin } from '../../utils/token';
